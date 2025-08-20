89c5fdfa3377b1cd5110fabea1f0363cd4a35ed8
ksulakshana02 <sulakshanakaveesha@gmail.com>
2025-08-20 11:20:30 +0530

    update terms & privacy

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,16 +1,105 @@
-import {PortableText} from 'next-sanity'
+import {PortableText, PortableTextReactComponents} from 'next-sanity'
 import {getMetadata, getTermsData} from "../../../sanity/api";
 import Nav from "@/components/Nav";
 import React from "react";
 import {Metadata} from "next";
 import {urlFor} from "../../../sanity/sanity";
 import AnimatedSection from "@/components/AnimatedSection";
+import Image from "next/image";
 
 
 // interface TermsAndConditionsData {
 //     title: string;
 //     body: any[];
 // }
+
+const portableTextComponents: Partial<PortableTextReactComponents> = {
+    // Handle block types (e.g., headings, normal text)
+    block: {
+        // normal: ({ children }) => (
+        //     <p className="mb-1">{children}</p>
+        // ),
+        normal: ({children}) => {
+            if (!children || (Array.isArray(children) && children.every(child => child === '\n' || !child))) {
+                return <p
+                    className="text-[#000000] text-sm sm:text-base whitespace-pre-line lg:text-lg leading-relaxed my-4 md:my-6"></p>;
+            }
+            return (
+                <p className="text-[#000000] text-sm sm:text-base whitespace-pre-line lg:text-lg leading-relaxed">
+                    {children}
+                </p>
+            );
+        },
+        h1: ({children}) => (
+            <h1 className="text-3xl font-bold mb-3">{children}</h1>
+        ),
+        h2: ({children}) => (
+            <h2 className="text-2xl font-bold mb-2">{children}</h2>
+        ),
+        h3: ({children}) => (
+            <h3 className="text-xl font-bold mb-1">{children}</h3>
+        ),
+        bullet: ({children}) => <li className="list-disc whitespace-pre-line ml-6">{children}</li>,
+        numbered: ({children}) => <li className="list-decimal whitespace-pre-line ml-6">{children}</li>,
+    },
+    // Handle list types (bullet and numbered lists)
+    list: {
+        bullet: ({children}) => (
+            <ul className="list-disc pl-6 mb-2 text-sm sm:text-base lg:text-lg">{children}</ul>
+        ),
+        number: ({children}) => (
+            <ol className="list-decimal pl-6 mb-2 text-sm sm:text-base lg:text-lg">{children}</ol>
+        ),
+    },
+    // Handle list items
+    listItem: {
+        bullet: ({children}) => (
+            <li className="mb-1 pl-6">{children}</li>
+        ),
+        number: ({children}) => (
+            <li className="mb-1 pl-6">{children}</li>
+        ),
+    },
+
+    marks: {
+        strong: ({children}) => <strong className="font-bold">{children}</strong>,
+        em: ({children}) => <em className="italic">{children}</em>,
+        link: ({children, value}) => (
+            <a
+                href={value.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 underline hover:text-blue-800"
+            >
+                {children}
+            </a>
+        ),
+    },
+    types: {
+        span: ({value}) => {
+            if (value.text === '\n') {
+                return <br/>;
+            }
+            return <span>{value.text}</span>;
+        },
+
+        image: ({value}) => {
+            const {alt, caption} = value;
+            return (
+                <figure>
+                    <Image
+                        src={urlFor(value).url()}
+                        alt={alt || "Image"}
+                        height={400}
+                        width={600}
+                        className="w-auto h-auto rounded-[10px] object-contain max-w-[700px]"
+                    />
+                    {caption && <figcaption className="text-sm text-center mt-2">{caption}</figcaption>}
+                </figure>
+            );
+        }
+    },
+};
 
 
 const TermsAndConditions = async () => {
@@ -28,7 +117,9 @@ const TermsAndConditions = async () => {
                 <div
                     className="font-light justify-left leading-12 px-4 sm:px-6 md:px-15 mt-6 text-[14px] sm:text-[16px] lg:text-[20px]">
                     <PortableText
-                        value={termsData.body || "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id ante eros. Donec congue eu mi non efficitur. Nunc vitae ultricies augue, at blandit tellus. Mauris eget sapien sed nisi bibendum convallis non eget leo. Curabitur vehicula a est quis commodo. Vestibulum faucibus a purus ac laoreet. Morbi tincidunt aliquam facilisis. Phasellus eget lobortis lorem. Proin quis semper ex. Pellentesque dignissim nulla tellus, lacinia porttitor tellus aliquam vitae. Fusce nec urna sit amet metus ullamcorper volutpat. Curabitur dapibus dolor ex, non gravida dui congue sed. Cras varius consectetur enim at mollis. Integer feugiat ex non vestibulum mollis."}/>
+                        value={termsData.body || ""}
+                        components={portableTextComponents}
+                    />
                 </div>
             </AnimatedSection>
         </div>
